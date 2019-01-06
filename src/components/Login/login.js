6cef7e2a614b7976220527a7d78a84b41fc31607
Sanul-Raskar <sanulraskar@gmail.com>
2019-01-06 13:16:48 +0530

    Added Google and Facebook SignIn buttons

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -5,13 +5,11 @@ import {
   TextInput,
   View,
   KeyboardAvoidingView,
-  Image,
   TouchableOpacity,
   StatusBar,
   ScrollView
 } from "react-native";
 import { StackActions, NavigationActions } from "react-navigation";
-import Home from "../Home/home";
 export default class login extends Component {
   static navigationOptions = {
     header: null
@@ -31,8 +29,7 @@ export default class login extends Component {
           <StatusBar barStyle="dark-content" backgroundColor="white" />
           <KeyboardAvoidingView style={styles.container} behavior="padding">
             <View style={styles.container}>
-              <Text style={styles.title}>Bikepool</Text>
-              <Image style={styles.logo} source={require("./bikepool.png")} />
+              <Text style={styles.title}>Login</Text>
               <TextInput
                 placeholder="Email"
                 keyboardType="email-address"
@@ -55,7 +52,21 @@ export default class login extends Component {
                 onPress={() => this.props.navigation.dispatch(resetAction)}
                 style={styles.loginButton}
               >
-                <Text style={styles.loginButtonText}>Login</Text>
+                <Text style={styles.ButtonText}>Login</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => this.props.navigation.dispatch(resetAction)}
+                style={styles.GoogleButton}
+              >
+                <Text style={styles.ButtonText}>Login with Google</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => this.props.navigation.dispatch(resetAction)}
+                style={styles.FaceBookButton}
+              >
+                <Text style={styles.ButtonText}>Login with Facebook</Text>
               </TouchableOpacity>
               <Text
                 style={{
@@ -99,7 +110,7 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: "#e6e6e6",
-    marginBottom: 12,
+    marginBottom: 14,
     height: 42,
     paddingHorizontal: 18,
     width: 300,
@@ -108,24 +119,37 @@ const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: "black",
     paddingVertical: 10,
+    marginBottom: 50,
+    width: 300,
+    borderRadius: 24
+  },
+  GoogleButton: {
+    backgroundColor: "rgb(211, 72, 54)",
+    paddingVertical: 10,
     marginBottom: 14,
     width: 300,
     borderRadius: 24
   },
-  loginButtonText: {
+  FaceBookButton: {
+    backgroundColor: "rgb(60,90,153)",
+    paddingVertical: 10,
+    marginBottom: 14,
+    width: 300,
+    borderRadius: 24
+  },
+  ButtonText: {
     fontSize: 22,
     textAlign: "center",
     height: 30,
     color: "white",
     fontWeight: "bold"
   },
-
   title: {
     fontFamily: "sans-serif-light",
     fontWeight: "bold",
     fontSize: 36,
-    color:"black",
-    marginBottom: 14,
-    marginTop: 6,
+    color: "black",
+    marginBottom: 20,
+    marginTop: 20
   }
 });
