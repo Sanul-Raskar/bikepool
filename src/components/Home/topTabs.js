61169aaf900202ff09bf74d1b5fe874820c0cf32
Sanul-Raskar <sanulraskar@gmail.com>
2019-06-08 19:10:27 +0530

    Added Flatlist to Rides screen

--- src/components/Home/topTabs.js
+++ src/components/Home/topTabs.js
@@ -2,20 +2,12 @@ import React, { Component } from "react";
 import {
   StyleSheet,
   View,
-  TouchableOpacity,
   ScrollView,
-  SafeAreaView
+  SafeAreaView,
+  FlatList
 } from "react-native";
 import { createMaterialTopTabNavigator } from "react-navigation";
-import {
-  Card,
-  CardItem,
-  Thumbnail,
-  Text,
-  Left,
-  Body,
-  Right
-} from "native-base";
+import { Card, CardItem, Thumbnail, Text, Left, Body } from "native-base";
 
 export default class topTabs extends Component {
   render() {
@@ -28,95 +20,83 @@ export default class topTabs extends Component {
 }
 
 export class PreviousRides extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      PreviousRides: [
+        {
+          id: "1",
+          Name: "Shubham Barmecha",
+          Type: "You offered ride",
+          Source: "VIIT College of Engineering,Pune",
+          Destination: "Shraddha Heritage, Morwadi,Pimpri",
+          Vehicle_Number: "MH 12 EV 1058",
+          Time: "Tue Dec 25 2018 07:30:00"
+        },
+        {
+          id: "2",
+          Name: "Kaustubh Sankpal",
+          Type: "He offered ride",
+          Source: "VIIT College of Engineering,Pune",
+          Destination: "Shraddha Heritage, Morwadi,Pimpri",
+          Vehicle_Number: "MH 12 EV 1058",
+          Time: "Tue Dec 25 2018 07:30:00"
+        },
+        {
+          id: "3",
+          Name: "Rohit Jagtap",
+          Type: "You offered ride",
+          Source: "VIIT College of Engineering,Pune",
+          Destination: "Shraddha Heritage, Morwadi,Pimpri",
+          Vehicle_Number: "MH 12 EV 1058",
+          Time: "Tue Dec 25 2018 07:30:00"
+        }
+      ]
+    };
+  }
+
+  RenderItem = ({ item }) => (
+    <Card style={{ marginBottom: 10 }}>
+      <CardItem>
+        <Left>
+          <Thumbnail
+            source={{
+              uri:
+                "https://facebook.github.io/react-native/docs/assets/favicon.png"
+            }}
+          />
+          <Body>
+            <Text style={styles.label}>{item.Name} </Text>
+            <Text note>{item.Type}</Text>
+          </Body>
+        </Left>
+      </CardItem>
+      <CardItem>
+        <Left>
+          <Body>
+            <Text style={styles.label}>Source:</Text>
+            <Text style={styles.subText}>{item.Source}</Text>
+            <Text style={styles.label}>Destination:</Text>
+            <Text style={styles.subText}>{item.Destination}</Text>
+            <Text style={styles.label}>Vehicle Number:</Text>
+            <Text style={styles.subText}>{item.Vehicle_Number}</Text>
+            <Text style={styles.label}>Time Stamp:</Text>
+            <Text style={styles.subText}>{item.Time}</Text>
+          </Body>
+        </Left>
+      </CardItem>
+    </Card>
+  );
+
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, padding: 20, backgroundColor: "white" }}>
         <ScrollView>
-          <Card>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Thumbnail source={require("../../assets/img/shubham.jpg")} />
-                <Body>
-                  <Text style={styles.label}>Shubham Barmecha</Text>
-                  <Text style={{ color: "white" }} note>
-                    You offered ride
-                  </Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Body>
-                  <Text style={styles.label}>Source</Text>
-                  <Text style={styles.subText}>Home</Text>
-                  <Text style={styles.label}>Destination</Text>
-                  <Text style={styles.subText}>
-                    Starbucks Coffee Mosaic Ground Floor West Across Fergusson
-                    College Gate, 2, Fergusson College Rd, Shivajinagar, Pune,
-                    Maharashtra 411004
-                  </Text>
-                  <Text style={styles.label}>Vehicle Number</Text>
-                  <Text style={styles.subText}>MH 12 EV 1058</Text>
-                  <Text style={styles.label}>Time Stamp</Text>
-                  <Text style={styles.subText}>Tue Dec 25 2018 07:30:00</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Thumbnail source={require("../../assets/img/kaustubh.jpg")} />
-                <Body>
-                  <Text style={styles.label}>Kaustubh Sankpal</Text>
-                  <Text style={{ color: "white" }} note>
-                    He offered ride
-                  </Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Body>
-                  <Text style={styles.label}>Source</Text>
-                  <Text style={styles.subText}>Home</Text>
-                  <Text style={styles.label}>Destination</Text>
-                  <Text style={styles.subText}>Work</Text>
-                  <Text style={styles.label}>Vehicle Number</Text>
-                  <Text style={styles.subText}>MH 12 AC 1753</Text>
-                  <Text style={styles.label}>Time Stamp</Text>
-                  <Text style={styles.subText}>Wed Dec 12 2018 07:30:00</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Thumbnail source={require("../../assets/img/andy.png")} />
-                <Body>
-                  <Text style={styles.label}>Aniruddha Tonge</Text>
-                  <Text style={{ color: "white" }} note>
-                    He offered ride
-                  </Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Body>
-                  <Text style={styles.label}>Source</Text>
-                  <Text style={styles.subText}>Home</Text>
-                  <Text style={styles.label}>Destination</Text>
-                  <Text style={styles.subText}>Work</Text>
-                  <Text style={styles.label}>Vehicle Number</Text>
-                  <Text style={styles.subText}>MH 12 AC 1753</Text>
-                  <Text style={styles.label}>Time Stamp</Text>
-                  <Text style={styles.subText}>Wed Dec 12 2018 07:30:00</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+          <FlatList
+            data={this.state.PreviousRides}
+            renderItem={this.RenderItem}
+            keyExtractor={item => item.id}
+          />
         </ScrollView>
       </View>
     );
@@ -126,49 +106,63 @@ export class PreviousRides extends Component {
 export class UpcomingRides extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      UpcomingRides: [
+        {
+          id: "1",
+          Name: "Shubham Barmecha",
+          Type: "You are offering ride",
+          Source: "VIIT College of Engineering,Pune",
+          Destination: "Shraddha Heritage, Morwadi,Pimpri",
+          Vehicle_Number: "MH 12 EV 1058",
+          Time: "Wed May 25 2019 07:30:00"
+        }
+      ]
+    };
   }
+
+  RenderItem = ({ item }) => (
+    <Card style={{ marginBottom: 10 }}>
+      <CardItem>
+        <Left>
+          <Thumbnail
+            source={{
+              uri:
+                "https://facebook.github.io/react-native/docs/assets/favicon.png"
+            }}
+          />
+          <Body>
+            <Text style={styles.label}>{item.Name} </Text>
+            <Text note>{item.Type}</Text>
+          </Body>
+        </Left>
+      </CardItem>
+      <CardItem>
+        <Left>
+          <Body>
+            <Text style={styles.label}>Source:</Text>
+            <Text style={styles.subText}>{item.Source}</Text>
+            <Text style={styles.label}>Destination:</Text>
+            <Text style={styles.subText}>{item.Destination}</Text>
+            <Text style={styles.label}>Vehicle Number:</Text>
+            <Text style={styles.subText}>{item.Vehicle_Number}</Text>
+            <Text style={styles.label}>Time Stamp:</Text>
+            <Text style={styles.subText}>{item.Time}</Text>
+          </Body>
+        </Left>
+      </CardItem>
+    </Card>
+  );
+
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, padding: 20, backgroundColor: "white" }}>
         <ScrollView>
-          <Card>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Thumbnail source={require("../../assets/img/vijay.jpg")} />
-                <Body>
-                  <Text style={styles.label}>Vijay Raskar</Text>
-                  <Text style={{ color: "white" }} note>
-                    You are offering ride
-                  </Text>
-                </Body>
-              </Left>
-              <Right>
-                <TouchableOpacity style={styles.trackButton}>
-                  <Text style={styles.ButtonText}>Track</Text>
-                </TouchableOpacity>
-              </Right>
-            </CardItem>
-            <CardItem style={{ backgroundColor: "rgb(38, 38, 38)" }}>
-              <Left>
-                <Body>
-                  <Text style={styles.label}>Source</Text>
-                  <Text style={styles.subText}>
-                    Narveer Tanaji Housing Society Ramling - Shirur Rd
-                    Shivrashka Colony Shirur, Maharashtra 412210
-                  </Text>
-                  <Text style={styles.label}>Destination</Text>
-                  <Text style={styles.subText}>
-                    Ranjangaon MIDC, Maharashtra 412220
-                  </Text>
-                  <Text style={styles.label}>Vehicle Number</Text>
-                  <Text style={styles.subText}>MH 12 EV 1058</Text>
-                  <Text style={styles.label}>Time Stamp</Text>
-                  <Text style={styles.subText}>Tue Dec 25 2018 07:30:00</Text>
-                </Body>
-              </Left>
-            </CardItem>
-          </Card>
+          <FlatList
+            data={this.state.UpcomingRides}
+            renderItem={this.RenderItem}
+            keyExtractor={item => item.id}
+          />
         </ScrollView>
       </View>
     );
@@ -201,24 +195,10 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 18,
-    fontWeight: "bold",
-    color: "white"
+    fontWeight: "bold"
   },
   subText: {
     fontSize: 14,
-    marginBottom: 20,
-    color: "white"
-  },
-  trackButton: {
-    backgroundColor: "white",
-    paddingVertical: 10,
-    width: 100,
-    borderRadius: 24
-  },
-  ButtonText: {
-    fontSize: 16,
-    textAlign: "center",
-    color: "black",
-    fontWeight: "bold"
+    marginBottom: 20
   }
 });
