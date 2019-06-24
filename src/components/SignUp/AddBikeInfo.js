98095aebdb6ecdb9dd956491e65eaccddf561120
Sanul-Raskar <sanulraskar@gmail.com>
2019-06-24 15:14:06 +0530

    Bike Info component | Added button

--- src/components/SignUp/AddBikeInfo.js
+++ src/components/SignUp/AddBikeInfo.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Picker } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Picker,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity
+} from "react-native";
 import FloatingLabelInput from "../FormAnimation/formAnimation";
 
 export default class AddBikeInfo extends Component {
@@ -123,153 +131,183 @@ export default class AddBikeInfo extends Component {
   };
 
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.container}>
-        <Text
-          style={{
-            fontSize: 16,
-            marginTop: 8,
-            color: "#666666",
-            zIndex: 10,
-            backgroundColor: "white",
-            width: 150,
-            marginBottom: 0,
-            marginLeft: 8
-          }}
-        >
-          Select Manufacturer
-        </Text>
-
-        <View
-          style={{
-            borderWidth: 1.5,
-            borderRadius: 8,
-            marginBottom: 8,
-            borderColor: "#dadce0",
-            marginTop: -8
-          }}
-        >
-          <Picker
-            selectedValue={this.state.manufacturer}
-            mode="dialog"
+        <StatusBar barStyle="dark-content" backgroundColor="white" />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text
             style={{
-              height: 50,
-              width: "100%",
-              borderColor: "#dadce0",
+              fontSize: 16,
+              marginTop: 8,
+              color: "#666666",
+              zIndex: 10,
+              backgroundColor: "white",
+              width: 150,
+              marginBottom: 0,
+              marginLeft: 8
+            }}
+          >
+            Select Manufacturer
+          </Text>
+
+          <View
+            style={{
               borderWidth: 1.5,
               borderRadius: 8,
-              marginTop: 0,
-              color: "#666666"
-            }}
-            onValueChange={itemValue =>
-              this.setState({
-                manufacturer: itemValue,
-                manufacturerSelected: true
-              })
-            }
-          >
-            <Picker.Item label="Select Manufacturer" value="" />
-            {this.state.bike.map((item, i) => {
-              return (
-                <Picker.Item
-                  key={i}
-                  label={item.manufacturer}
-                  value={item.manufacturer}
-                />
-              );
-            })}
-          </Picker>
-        </View>
-
-        {this.state.manufacturerSelected && this.filterArray()}
-
-        <Text
-          style={{
-            fontSize: 16,
-            marginTop: 8,
-            color: "#666666",
-            zIndex: 10,
-            backgroundColor: "white",
-            width: 150,
-            marginBottom: 0,
-            marginLeft: 8
-          }}
-        >
-          Bike color
-        </Text>
-
-        <View
-          style={{
-            borderWidth: 1.5,
-            borderRadius: 8,
-            marginBottom: 8,
-            borderColor: "#dadce0",
-            marginTop: -8
-          }}
-        >
-          <Picker
-            selectedValue={this.state.bikeColor}
-            mode="dialog"
-            style={{
-              height: 50,
-              width: "100%",
+              marginBottom: 8,
               borderColor: "#dadce0",
+              marginTop: -8
+            }}
+          >
+            <Picker
+              selectedValue={this.state.manufacturer}
+              mode="dialog"
+              style={{
+                height: 50,
+                width: "100%",
+                borderColor: "#dadce0",
+                borderWidth: 1.5,
+                borderRadius: 8,
+                marginTop: 0,
+                color: "#666666"
+              }}
+              onValueChange={itemValue =>
+                this.setState({
+                  manufacturer: itemValue,
+                  manufacturerSelected: true
+                })
+              }
+            >
+              <Picker.Item label="Select Manufacturer" value="" />
+              {this.state.bike.map((item, i) => {
+                return (
+                  <Picker.Item
+                    key={i}
+                    label={item.manufacturer}
+                    value={item.manufacturer}
+                  />
+                );
+              })}
+            </Picker>
+          </View>
+
+          {this.state.manufacturerSelected && this.filterArray()}
+
+          <Text
+            style={{
+              fontSize: 16,
+              marginTop: 8,
+              color: "#666666",
+              zIndex: 10,
+              backgroundColor: "white",
+              width: 80,
+              marginBottom: 0,
+              marginLeft: 8
+            }}
+          >
+            Bike color
+          </Text>
+
+          <View
+            style={{
               borderWidth: 1.5,
               borderRadius: 8,
-              marginTop: 0,
-              color: "#666666"
+              marginBottom: 8,
+              borderColor: "#dadce0",
+              marginTop: -8
             }}
-            onValueChange={itemValue =>
-              this.setState({
-                manufacturer: itemValue,
-                manufacturerSelected: true
-              })
-            }
           >
-            <Picker.Item label="Select Manufacturer" value="" />
-            {this.state.bike.map((item, i) => {
-              return (
-                <Picker.Item
-                  key={i}
-                  label={item.manufacturer}
-                  value={item.manufacturer}
-                />
-              );
-            })}
-          </Picker>
-        </View>
+            <Picker
+              selectedValue={this.state.bikeColor}
+              mode="dialog"
+              style={{
+                height: 50,
+                width: "100%",
+                borderColor: "#dadce0",
+                borderWidth: 1.5,
+                borderRadius: 8,
+                marginTop: 0,
+                color: "#666666"
+              }}
+              onValueChange={itemValue =>
+                this.setState({
+                  bikeColor: itemValue
+                })
+              }
+            >
+              <Picker.Item label="Select Bike Color" value="" />
+              {this.state.Colors.map((item, i) => {
+                return <Picker.Item key={i} label={item} value={item} />;
+              })}
+            </Picker>
+          </View>
 
-        <FloatingLabelInput
-          label="Vehicle License Number"
-          value={this.state.vehicleLicense}
-          border={this.state.border_Color_vehicleLicense}
-          onChangeText={this.handleVehicleLicenseChange}
-          keyboardLayout="default"
-          fontColor={this.state.vehicleLicense_font_color}
-          onFocusBorder={this.state.vehicleLicense_onFocus_border}
-        />
-        {this.state.vehicleLicenseError !== "" && (
-          <Text style={styles.error}>
-            <Icon name="alert-circle" color="red" size={16} />{" "}
-            {this.state.vehicleLicenseError}
-          </Text>
-        )}
+          <FloatingLabelInput
+            label="Vehicle License Number"
+            value={this.state.vehicleLicense}
+            border={this.state.border_Color_vehicleLicense}
+            onChangeText={this.handleVehicleLicenseChange}
+            keyboardLayout="default"
+            fontColor={this.state.vehicleLicense_font_color}
+            onFocusBorder={this.state.vehicleLicense_onFocus_border}
+          />
+          {this.state.vehicleLicenseError !== "" && (
+            <Text style={styles.error}>
+              <Icon name="alert-circle" color="red" size={16} />{" "}
+              {this.state.vehicleLicenseError}
+            </Text>
+          )}
 
-        <FloatingLabelInput
-          label="Driving License Number"
-          value={this.state.drivingLicense}
-          border={this.state.border_Color_drivingLicense}
-          onChangeText={this.handleDrivingLicenseChange}
-          keyboardLayout="default"
-          fontColor={this.state.drivingLicense_font_color}
-          onFocusBorder={this.state.drivingLicense_onFocus_border}
-        />
-        {this.state.drivingLicenseError !== "" && (
-          <Text style={styles.error}>
-            <Icon name="alert-circle" color="red" size={16} />{" "}
-            {this.state.drivingLicenseError}
-          </Text>
-        )}
+          <FloatingLabelInput
+            label="Driving License Number"
+            value={this.state.drivingLicense}
+            border={this.state.border_Color_drivingLicense}
+            onChangeText={this.handleDrivingLicenseChange}
+            keyboardLayout="default"
+            fontColor={this.state.drivingLicense_font_color}
+            onFocusBorder={this.state.drivingLicense_onFocus_border}
+          />
+          {this.state.drivingLicenseError !== "" && (
+            <Text style={styles.error}>
+              <Icon name="alert-circle" color="red" size={16} />{" "}
+              {this.state.drivingLicenseError}
+            </Text>
+          )}
+
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "flex-end"
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#1a73e8",
+                marginBottom: 50,
+                borderRadius: 10,
+                marginTop: 60
+              }}
+              /* onPress={this.validate}*/
+              onPress={() => navigate("AccountCreated")}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: "white",
+                  paddingTop: 10,
+                  paddingBottom: 12,
+                  paddingHorizontal: 18,
+                  textAlign: "center"
+                }}
+              >
+                Next
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     );
   }
